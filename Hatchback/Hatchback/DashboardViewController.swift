4a2583f4f3110bd7a342f11b46912b51c0fba85f
Alex Santarelli <alexszandc@gmail.com>
2017-07-11 12:58:29 -0400

    Initial Commit and Setup.

---
+++ Hatchback/Hatchback/DashboardViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Hatchback
+//
+//  Created by Alex Santarelli on 7/11/17.
+//  Copyright © 2017 Alex Santarelli. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
