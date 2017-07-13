fd064311c9ab8877c6122b479ac11233698223c7
Alex Santarelli <alexszandc@gmail.com>
2017-07-13 14:44:47 -0400

    Added base configurations and styles for development testing.:

--- Hatchback/Hatchback/AppDelegate.swift
+++ Hatchback/Hatchback/AppDelegate.swift
@@ -22,25 +22,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        print("APPDELEGATE -- applicationWillResignActive \t\t- app is transtitioning into the background state")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("APPDELEGATE -- applicationDidEnterBackground \t- app entering into the background queue.")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("APPDELEGATE -- applicationWillEnterForeground \t- app transitioning from background to Foreground state")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("APPDELEGATE -- applicationDidBecomeActive \t\t- app became active ")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("APPDELEGATE -- applicationWillTerminate \t\t- the app is about to be terminated from memory")
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        if UIApplication.shared.canOpenURL(URL(string: "comgooglemaps:")!) {
+        }
+        
+        return false
     }
 
-
+    // this will be used for opening spotify, apple music, google maps, and apple maps
+    //    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+    //
+    //    }
 }
 
--- Hatchback/Hatchback/DriveViewController.swift
+++ Hatchback/Hatchback/DriveViewController.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class DriveViewController: UIViewController {
+    
+    var timer:Timer = Timer()
+    var startTime:TimeInterval = 0.0
+    
+    @IBOutlet weak var timerLabel:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.title = "Distraction Free"
+        
+        self.setTimer()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +29,90 @@ class DriveViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func openMusic(_ sender: Any) {
+        let appleMusicLink = "music://"
+        let spotifyMusicLink = "spotify://"
+        openApp(appURLString: spotifyMusicLink)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
 
+    @IBAction func openMaps(_ sender: Any) {
+        let mapsLink = "maps://"
+        let googleMapsLink = "comgooglemaps://"
+        openApp(appURLString: googleMapsLink)
+    }
+    
+    @IBAction func endButton(_ sender: Any) {
+        self.endTimer()
+        
+        //bring up the ui for ending a drive
+    }
+
+    
+    func openApp(appURLString: String) {
+        if let url = URL(string: appURLString), UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: {(success: Bool)
+                in if success {
+                    print("Launching \(url)")
+                
+                } else {
+                    print("There was an error")
+                }})
+        }
+    }
+    
+    //timer related functions
+    func setTimer() {
+        let updateSelector : Selector = #selector(DriveViewController.updateTimer)
+        timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: updateSelector, userInfo: nil, repeats: true) //was 0.01 // should be more if it does not affect performance
+        startTime = Date.timeIntervalSinceReferenceDate
+    }
+    
+    func updateTimer() {
+        let currentTime = Date.timeIntervalSinceReferenceDate
+        
+        //Find the difference between current time and start time.
+        var elapsedTime: TimeInterval = currentTime - startTime
+        
+        //calculate the minutes in elapsed time.
+        let minutes = UInt8(elapsedTime / 60.0)
+        elapsedTime -= (TimeInterval(minutes) * 60)
+        
+        //calculate the seconds in elapsed time.
+        let seconds = UInt8(elapsedTime)
+        elapsedTime -= TimeInterval(seconds)
+        
+        
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        
+        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
+        timerLabel.text = "\(strMinutes):\(strSeconds)"
+    }
+    
+    func endTimer() {
+        let totalTime = Date.timeIntervalSinceReferenceDate - startTime
+        print("The Total Time for that trip was: \(totalTime)")
+        
+        //stopping the timer
+        timer.invalidate()
+        
+    }
+    
+    //spotify open url
+    //spoitfy://  -> brings you to the your library portion of your spotify
+    //spotify:app:browse //if we wanted to go deeper this is somewhat of how it would look
+    
+    //google maps
+    //comgooglemaps://
+    
+    //apple music 
+    //music://
+    
+    //apple maps 
+    //maps://
 }
