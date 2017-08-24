c5be4304e796c75341aa996931c045c13e5d359a
Alex Santarelli <alexszandc@gmail.com>
2017-08-24 13:26:55 -0400

    Added all nessecary files, and app icons.

---
+++ Hatchback/Hatchback/SettingsTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsTableViewCell.swift
+//  Hatchback
+//
+//  Created by Alex Santarelli on 8/24/17.
+//  Copyright © 2017 Alex Santarelli. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var settingsTitleLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
